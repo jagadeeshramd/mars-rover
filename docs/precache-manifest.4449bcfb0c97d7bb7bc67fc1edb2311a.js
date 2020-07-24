3eb433f51bf54fd8fda42dff80b89dfdbd6fb752
jagadeeshramd <jagadeeshramd@gmail.com>
2020-07-24 18:57:49 -0500

    Add docs folder

---
+++ docs/precache-manifest.4449bcfb0c97d7bb7bc67fc1edb2311a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77776cd84f7c8e9512e749c8ca8abab",
+    "url": "/mars-rover/index.html"
+  },
+  {
+    "revision": "f4f58aaf12eb1baf093c",
+    "url": "/mars-rover/static/css/main.f3808c60.chunk.css"
+  },
+  {
+    "revision": "53a636c5fe1aada0e045",
+    "url": "/mars-rover/static/js/2.a2e622e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mars-rover/static/js/2.a2e622e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f58aaf12eb1baf093c",
+    "url": "/mars-rover/static/js/main.e2e6085d.chunk.js"
+  },
+  {
+    "revision": "076fcc8ce0acf1a46f36",
+    "url": "/mars-rover/static/js/runtime-main.17681532.js"
+  }
+]);
